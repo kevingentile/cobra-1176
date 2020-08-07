6dc8004070d3c350f038257b70b478a8a8321754
Kevin Gentile <kgentile@protonmail.com>
2020-08-06 23:34:40 -0400

    testcmd to stdout

--- cmd/testCmd.go
+++ cmd/testCmd.go
@@ -1,11 +1,15 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
 
 var testCmd = &cobra.Command{
 	Use:   "test",
 	Short: "experimental test command",
 	Run: func(cmd *cobra.Command, args []string) {
-		cmd.Help()
+		fmt.Println("test command")
 	},
 }
